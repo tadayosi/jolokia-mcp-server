b20556a03aff77ab34322a2acb0ff1c87f7821b2
Grzegorz Grzybek <gr.grzybek@gmail.com>
2025-11-04 08:39:43 +0100

    chore(deps): Adjust to Jolokia Client changes from 2.4.0+

--- server/src/main/java/org/jolokia/mcp/JolokiaClient.java
+++ server/src/main/java/org/jolokia/mcp/JolokiaClient.java
@@ -23,31 +23,30 @@ import javax.management.MalformedObjectNameException;
 import jakarta.enterprise.context.ApplicationScoped;
 
 import org.eclipse.microprofile.config.inject.ConfigProperty;
-import org.jolokia.client.J4pClient;
-import org.jolokia.client.exception.J4pException;
-import org.jolokia.client.request.J4pExecRequest;
-import org.jolokia.client.request.J4pExecResponse;
-import org.jolokia.client.request.J4pListRequest;
-import org.jolokia.client.request.J4pListResponse;
-import org.jolokia.client.request.J4pReadRequest;
-import org.jolokia.client.request.J4pReadResponse;
-import org.jolokia.client.request.J4pRequest;
-import org.jolokia.client.request.J4pWriteRequest;
-import org.jolokia.client.request.J4pWriteResponse;
+import org.jolokia.client.EscapeUtil;
+import org.jolokia.client.exception.JolokiaException;
+import org.jolokia.client.request.JolokiaExecRequest;
+import org.jolokia.client.request.JolokiaListRequest;
+import org.jolokia.client.request.JolokiaReadRequest;
+import org.jolokia.client.request.JolokiaWriteRequest;
+import org.jolokia.client.response.JolokiaExecResponse;
+import org.jolokia.client.response.JolokiaListResponse;
+import org.jolokia.client.response.JolokiaReadResponse;
+import org.jolokia.client.response.JolokiaWriteResponse;
 import org.jolokia.json.JSONObject;
 
 @ApplicationScoped
 public class JolokiaClient implements JolokiaService {
 
-    J4pClient jolokiaClient;
+    org.jolokia.client.JolokiaClient jolokiaClient;
     MBeanListCache mbeanListCache;
 
     public JolokiaClient(@ConfigProperty(name = "jolokia.mcp.url", defaultValue = "http://localhost:8778/jolokia")
                          String jolokiaUrl) {
-        jolokiaClient = new J4pClient(jolokiaUrl);
+        jolokiaClient = new org.jolokia.client.JolokiaClientBuilder().url(jolokiaUrl).build();
     }
 
-    public List<String> listMBeans() throws J4pException {
+    public List<String> listMBeans() throws JolokiaException {
         if (mbeanListCache != null && mbeanListCache.isValid()) {
             return mbeanListCache.getMBeans();
         }
@@ -61,44 +60,44 @@ public class JolokiaClient implements JolokiaService {
         return result;
     }
 
-    public JSONObject listOperations(String mbean) throws J4pException {
+    public JSONObject listOperations(String mbean) throws JolokiaException {
         return getFromMBean(mbean, "op");
     }
 
-    public JSONObject listAttributes(String mbean) throws J4pException {
+    public JSONObject listAttributes(String mbean) throws JolokiaException {
         return getFromMBean(mbean, "attr");
     }
 
-    JSONObject getFromMBean(String mbean, String key) throws J4pException {
+    JSONObject getFromMBean(String mbean, String key) throws JolokiaException {
         JSONObject mbeanInfo = list(toPath(mbean));
         return (JSONObject) mbeanInfo.getOrDefault(key, new JSONObject());
     }
 
     static String toPath(String mbean) {
-        return J4pRequest.escape(mbean).replaceFirst(":", "/");
+        return EscapeUtil.escape(mbean).replaceFirst(":", "/");
     }
 
-    JSONObject list(String path) throws J4pException {
-        J4pListRequest req = new J4pListRequest(path);
-        J4pListResponse resp = jolokiaClient.execute(req);
+    JSONObject list(String path) throws JolokiaException {
+        JolokiaListRequest req = new JolokiaListRequest(path);
+        JolokiaListResponse resp = jolokiaClient.execute(req);
         return resp.getValue();
     }
 
-    public Optional<Object> read(String mbean, String attr) throws J4pException, MalformedObjectNameException {
-        J4pReadRequest req = new J4pReadRequest(mbean, attr);
-        J4pReadResponse resp = jolokiaClient.execute(req);
+    public Optional<Object> read(String mbean, String attr) throws JolokiaException, MalformedObjectNameException {
+        JolokiaReadRequest req = new JolokiaReadRequest(mbean, attr);
+        JolokiaReadResponse resp = jolokiaClient.execute(req);
         return Optional.ofNullable(resp.getValue());
     }
 
-    public Optional<Object> write(String mbean, String attr, Object value) throws J4pException, MalformedObjectNameException {
-        J4pWriteRequest req = new J4pWriteRequest(mbean, attr, value);
-        J4pWriteResponse resp = jolokiaClient.execute(req);
+    public Optional<Object> write(String mbean, String attr, Object value) throws JolokiaException, MalformedObjectNameException {
+        JolokiaWriteRequest req = new JolokiaWriteRequest(mbean, attr, value);
+        JolokiaWriteResponse resp = jolokiaClient.execute(req);
         return Optional.ofNullable(resp.getValue());
     }
 
-    public Optional<Object> exec(String mbean, String op, Object... args) throws J4pException, MalformedObjectNameException {
-        J4pExecRequest req = new J4pExecRequest(mbean, op, args);
-        J4pExecResponse resp = jolokiaClient.execute(req);
+    public Optional<Object> exec(String mbean, String op, Object... args) throws JolokiaException, MalformedObjectNameException {
+        JolokiaExecRequest req = new JolokiaExecRequest(mbean, op, args);
+        JolokiaExecResponse resp = jolokiaClient.execute(req);
         return Optional.ofNullable(resp.getValue());
     }
 }
